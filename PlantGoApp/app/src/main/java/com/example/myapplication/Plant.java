6a2c1fa9474f5d5ef7a1c20ac842a78a36bda51b
David GOLAY <dg447135@iut21.u-bourgogne.fr>
2023-03-15 08:48:49 +0100

    refactored files & added plant list

---
+++ PlantGoApp/app/src/main/java/com/example/myapplication/Plant.java
@@ -0,0 +1,8 @@
+package com.example.myapplication;
+
+public class Plant {
+    public String name;
+    public String size;
+    public String resistance;
+    public String lightNeeds;
+}
--- PlantGoApp/app/src/main/java/com/example/myapplication/Rando.java
+++
@@ -1,6 +0,0 @@
-package com.example.myapplication;
-
-public class Rando {
-    public String nomRando;
-    public String dureeRando;
-}
