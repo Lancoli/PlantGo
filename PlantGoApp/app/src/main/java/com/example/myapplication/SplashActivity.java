dcacf5bb47562b2036efcc25a0df8d21ef07eac4
Just1Sornay <72704381+Just1Sornay@users.noreply.github.com>
2023-03-17 09:58:25 +0100

    ++

--- PlantGoApp/app/src/main/java/com/example/myapplication/SplashActivity.java
+++ PlantGoApp/app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -4,18 +4,45 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
 
 public class SplashActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
-        handleRedirectLogin();
+        initLoaderAnimation();
+
+        CountDownTimer timer = new CountDownTimer(1000, 1000) {
+            @Override
+            public void onTick(long l) {
+            }
+
+            @Override
+            public void onFinish() {
+                handleRedirectLogin();
+            }
+        };
+        timer.start();
     }
 
     public void handleRedirectLogin() {
         Intent intent = new Intent(this, LoginActivity.class);
         startActivity(intent);
+    }
+
+    public void initLoaderAnimation() {
+        RotateAnimation rotate = new RotateAnimation(0, 360,
+                Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF,
+                0.5f);
+
+        rotate.setDuration(4000);
+        rotate.setRepeatCount(Animation.INFINITE);
+        ImageView loader = findViewById(R.id.loader);
+        loader.setAnimation(rotate);
     }
 }
