62c6853c3cc7560209fd80e8c528c6db2a4cabda
David GOLAY <dg447135@iut21.u-bourgogne.fr>
2023-04-03 14:36:13 +0200

    added error message : empty pseudo

--- PlantGoApp/app/src/main/java/com/example/myapplication/activity/LoginActivity.java
+++ PlantGoApp/app/src/main/java/com/example/myapplication/activity/LoginActivity.java
@@ -3,9 +3,11 @@ package com.example.myapplication.activity;
 import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.android.volley.Request;
@@ -18,14 +20,23 @@ import com.example.myapplication.R;
 
 public class LoginActivity extends AppCompatActivity {
 
+    TextView pseudonymeEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         TextView connectBtn = (TextView) findViewById(R.id.connectButton);
+        pseudonymeEditText = findViewById(R.id.pseudo_input);
     }
 
     public void onClickSignUp(View view) {
+        // on vérifie si le pseudo est rempli
+        String pseudonyme = pseudonymeEditText.getText().toString();
+        if (TextUtils.isEmpty(pseudonyme)) {
+            Toast.makeText(this, "Le champ pseudonyme est obligatoire", Toast.LENGTH_SHORT).show();
+            return;
+        }
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
         makeRequest();
