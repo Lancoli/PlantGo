fe2523a6a4211ab53c13c905afd8bd18dd62c98b
Just1Sornay <72704381+Just1Sornay@users.noreply.github.com>
2023-04-03 17:32:23 +0200

    camera

--- PlantGoApp/app/src/main/java/com/example/myapplication/activity/AddPlantActivity.java
+++ PlantGoApp/app/src/main/java/com/example/myapplication/activity/AddPlantActivity.java
@@ -1,18 +1,37 @@
 package com.example.myapplication.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.FileProvider;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
-import android.widget.LinearLayout;
+import android.widget.ImageView;
 
 import com.example.myapplication.R;
 import com.example.myapplication.business.plant.Plant;
 import com.example.myapplication.storage.DBHandler;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 public class AddPlantActivity extends AppCompatActivity {
     public static final int RESULT_OK = 1;
+
+    private static final int BACKUP_TAKE_PHOTO = 1;
+
+    private Button takePhotoButton;
+    private ImageView photoImage;
+    private String photoPath = null;
 
     EditText inputName;
 
@@ -20,9 +39,49 @@ public class AddPlantActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_plant);
+        initActivity();
+    }
 
-        inputName = (EditText)findViewById(R.id.inputName);
-        LinearLayout photoButton = (LinearLayout) this.findViewById(R.id.photoButton);
+    private void initActivity() {
+        takePhotoButton = (Button) findViewById(R.id.takePhotoButton);
+        photoImage = (ImageView) findViewById(R.id.photoImage);
+        initTakePhoto();
+    }
+
+    private void initTakePhoto() {
+        takePhotoButton.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                handleTakePhoto();
+            }
+        });
+    }
+
+    private void handleTakePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            String time = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+            File photoDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+            try {
+                File photoFile = File.createTempFile("photo_"+time, ".jpg", photoDir);
+                photoPath = photoFile.getAbsolutePath();
+                Uri photoUri = FileProvider.getUriForFile(AddPlantActivity.this, AddPlantActivity.this.getApplicationContext().getPackageName()+".provider", photoFile);
+                intent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(intent, BACKUP_TAKE_PHOTO);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode==BACKUP_TAKE_PHOTO && resultCode==RESULT_OK) {
+            Bitmap imageBitmap = BitmapFactory.decodeFile(photoPath);
+            photoImage.setImageBitmap(imageBitmap);
+        }
     }
 
     public void onClickValidatePlant(View view) {
--- PlantGoApp/app/src/main/java/com/example/myapplication/activity/LoginActivity.java
+++ PlantGoApp/app/src/main/java/com/example/myapplication/activity/LoginActivity.java
@@ -8,18 +8,9 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 import android.widget.Toast;
-
-
-import com.android.volley.Request;
-import com.android.volley.Response;
-import com.android.volley.RequestQueue;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.StringRequest;
-import com.android.volley.toolbox.Volley;
 import com.example.myapplication.R;
 
 public class LoginActivity extends AppCompatActivity {
-
     TextView pseudonymeEditText;
 
     @Override
@@ -39,30 +30,5 @@ public class LoginActivity extends AppCompatActivity {
         }
         Intent intent = new Intent(this, HomeActivity.class);
         startActivity(intent);
-        makeRequest();
-    }
-
-    public void makeRequest() {
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
-        String url = "https://www.google.com";
-
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        System.out.println("Response is: " + response.substring(0,500));
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("Volly Error", error.toString());
-            }
-        });
-
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
     }
 }
